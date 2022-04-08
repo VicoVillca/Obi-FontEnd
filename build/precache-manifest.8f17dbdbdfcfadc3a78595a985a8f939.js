b9dc21e87cb3016a544ee4f367a49ca4ce4fc387
VicoVillca <vicovillca@hotmail.com>
2022-04-08 10:13:02 -0400

    push with build

---
+++ build/precache-manifest.8f17dbdbdfcfadc3a78595a985a8f939.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eafb9132f9be7bfa2ec96ccbbba4e563",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cca8248798a0a836d640",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "cca8248798a0a836d640",
+    "url": "/static/js/2.d471d0cb.chunk.js"
+  },
+  {
+    "revision": "5cc0f0e1b2eb6d616a2c05f00973517a",
+    "url": "/static/js/2.d471d0cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aaf05c1796f4233704b",
+    "url": "/static/js/main.3136da65.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/static/js/main.3136da65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bcc694f6ded8dcea1fc",
+    "url": "/static/js/runtime-main.38554731.js"
+  },
+  {
+    "revision": "bab128f4c893dd9f70ea3a29c4f2eb11",
+    "url": "/static/media/icpc_logo.bab128f4.jpg"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
